2f18ce156317db6604b96974ecabe718bd5cdd26
Julian Prieto <prietojulian2003@gmail.com>
2023-04-10 19:37:18 -0300

    Create clase_1.py

---
+++ clase_1.py
@@ -0,0 +1,20 @@
+# Crear funciones para calcular area y perimetro
+# de un cuadrado, de un rectangulo y de una circunferencia
+
+def area_cuadrado(lado):
+    return lado*lado
+
+def perimetro_cuadrado(lado):
+    return lado*4
+
+def area_rectangulo(lado_a, lado_b):
+    return lado_a * lado_b
+
+def perimetro_rectangulo(lado_a, lado_b):
+    return lado_a*2 + lado_b*2
+
+def area_circunferencia(radio):
+    return 3.14*radio**2
+
+def perimetro_circunferencia(radio):
+    return 2*3.14*radio
